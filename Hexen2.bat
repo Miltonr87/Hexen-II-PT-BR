a8533305884701c13a8e1a64de36f0fb9a3fcd4d
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-03-06 18:27:26 -0300

    Introdução e quebra-cabeças
    Todos foram traduzidos do jogo original

--- Hexen2.bat
+++ Hexen2.bat
@@ -14,7 +14,7 @@ echo  I_I I_I I__I /_/  \_\ I__I I_I  \_I  _I_I__I_I_
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo Bem Vindo ao Hexen 2. Escolha uma opcao para iniciar:
 echo 1: Hexen 2
-echo 2: Hexen 2: Portal of Praevus
+echo 2: Hexen 2: Portal de Praevus
 echo 3: Sair
 set /p choice="Enter:"
 if "%choice%"=="1" goto h2
